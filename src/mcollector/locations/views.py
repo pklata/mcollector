efe552ceec79ca739c2b866238e2aeb47b51b363
pklata <pawelklata@gmail.com>
2021-07-27 22:01:45 +0200

    locals views

--- src/mcollector/locations/views.py
+++ src/mcollector/locations/views.py
@@ -34,7 +34,23 @@ class BuildingCreate(BaseModel):
     county: Optional[str]
 
 
-class BuildingIdResponse(BaseModel):
+class LocalPresentation(BaseModel):
+    id: Optional[int] = None
+    number: Optional[int] = None
+    description: Optional[str] = None
+
+
+class LocalUpdate(BaseModel):
+    number: Optional[int] = None
+    description: Optional[str] = None
+
+
+class LocalCreate(BaseModel):
+    number: int
+    description: Optional[str] = None
+
+
+class IdResponse(BaseModel):
     id: int
 
 
@@ -51,7 +67,7 @@ async def get_building(building_id: int) -> Dict[str, Any]:
         raise HTTPException(status_code=404, detail=e.message)
 
 
-@app.post("/building", response_model=BuildingIdResponse)
+@app.post("/building", response_model=IdResponse)
 async def add_building(new_building: BuildingCreate) -> Dict[str, int]:
     try:
         return await service.add_building(
@@ -61,7 +77,7 @@ async def add_building(new_building: BuildingCreate) -> Dict[str, int]:
         raise HTTPException(status_code=404, detail=e.message)
 
 
-@app.patch("/building/{building_id}", response_model=BuildingIdResponse)
+@app.patch("/building/{building_id}", response_model=IdResponse)
 async def update_building(
     building_update: BuildingUpdate, building_id: int
 ) -> Dict[str, int]:
@@ -77,5 +93,46 @@ async def update_building(
 async def delete_building(building_id: int) -> Dict[str, int]:
     try:
         return await service.delete_building(building_id, uow_factory())
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+
+
+@app.get("/building/{building_id}/local", response_model=List[LocalPresentation])
+async def list_locals(building_id: int) -> List[Dict[str, Any]]:
+    return await service.list_locals(building_id, uow_factory())
+
+
+@app.get("/local/{local_id}", response_model=LocalPresentation)
+async def get_local(local_id: int) -> Dict[str, Any]:
+    try:
+        return await service.get_local(local_id, uow_factory())
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+
+
+@app.post("/building/{building_id}/local", response_model=IdResponse)
+async def add_local(new_local: LocalCreate, building_id: int) -> Dict[str, int]:
+    try:
+        return await service.add_local(
+            building_id, new_local.dict(exclude_unset=True), uow_factory()
+        )
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+
+
+@app.patch("/local/{local_id}", response_model=IdResponse)
+async def update_local(local_update: LocalUpdate, local_id: int) -> Dict[str, int]:
+    try:
+        return await service.update_local(
+            local_id, local_update.dict(exclude_unset=True), uow_factory()
+        )
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+
+
+@app.delete("/local/{local_id}")
+async def delete_local(local_id: int) -> Dict[str, int]:
+    try:
+        return await service.delete_local(local_id, uow_factory())
     except NotFoundError as e:
         raise HTTPException(status_code=404, detail=e.message)
--- tests/test_locations/test_location_views.py
+++ tests/test_locations/test_location_views.py
@@ -1,7 +1,5 @@
 import pytest
 
-from mcollector.locations.models import Building
-
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("recreate_db")
@@ -56,6 +54,19 @@ class TestBuildingViews:
             response = await app.post("/building", json=payload)
         assert response.json() == {"id": 1}
         assert response.status_code == 200
+        async with async_app as app:
+            response = await app.get("/building")
+        assert response.status_code == 200
+        assert response.json() == [
+            {
+                "address": "Towarowa 365",
+                "city": "Warszawa",
+                "country": "Polska",
+                "county": "mazowieckie",
+                "id": 1,
+                "zip_code": "02-200",
+            }
+        ]
 
     async def test_update_building(self, session, async_app, building):
         session.add(building(id=1))
@@ -68,14 +79,19 @@ class TestBuildingViews:
             response = await app.patch("/building/1", json=payload)
         assert response.status_code == 200
         assert response.json() == {"id": 1}
-        assert await session.get(Building, 1) == Building(
-            address="Graniczna 17",
-            city="Warszawa",
-            country="Anglia",
-            county="mazowieckie",
-            id=1,
-            zip_code="02-200",
-        )
+        async with async_app as app:
+            response = await app.get("/building")
+        assert response.status_code == 200
+        assert response.json() == [
+            {
+                "address": "Graniczna 17",
+                "city": "Warszawa",
+                "country": "Anglia",
+                "county": "mazowieckie",
+                "id": 1,
+                "zip_code": "02-200",
+            }
+        ]
 
     async def test_update_not_existing_building(self, async_app):
         payload = {
@@ -94,9 +110,95 @@ class TestBuildingViews:
             response = await app.delete("/building/1")
         assert response.status_code == 200
         assert response.json() == {}
+        async with async_app as app:
+            response = await app.get("/building")
+        assert response.status_code == 200
+        assert response.json() == []
 
     async def test_delete_not_existing_building(self, async_app):
         async with async_app as app:
             response = await app.delete("/building/1")
         assert response.status_code == 404
         assert response.json() == {"detail": "Building with id: 1 not found."}
+
+    @pytest.mark.usefixtures("locals_setup")
+    async def test_get_locals(self, async_app):
+        async with async_app as app:
+            response = await app.get("/building/1/local")
+        assert response.status_code == 200
+        assert response.json() == [
+            {"description": None, "id": 2, "number": 2},
+            {"description": None, "id": 1, "number": 7},
+        ]
+
+    @pytest.mark.usefixtures("locals_setup")
+    async def test_get_local(self, async_app):
+        async with async_app as app:
+            response = await app.get("/local/1")
+        assert response.status_code == 200
+        assert response.json() == {"description": None, "id": 1, "number": 7}
+
+    async def test_get_not_existing_local(self, async_app):
+        async with async_app as app:
+            response = await app.get("/local/1")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Local with id: 1 not found."}
+
+    @pytest.mark.usefixtures("locals_setup")
+    async def test_create_local(self, async_app):
+        payload = {"number": "13"}
+        async with async_app as app:
+            response = await app.post("/building/1/local", json=payload)
+        assert response.json() == {"id": 3}
+        assert response.status_code == 200
+        async with async_app as app:
+            response = await app.get("/building/1/local")
+        assert response.status_code == 200
+        assert response.json() == [
+            {"description": None, "id": 2, "number": 2},
+            {"description": None, "id": 1, "number": 7},
+            {"description": None, "id": 3, "number": 13},
+        ]
+
+    @pytest.mark.usefixtures("locals_setup")
+    async def test_update_local(self, async_app):
+        payload = {
+            "number": "17",
+        }
+        async with async_app as app:
+            response = await app.patch("/local/1", json=payload)
+        assert response.status_code == 200
+        assert response.json() == {"id": 1}
+        async with async_app as app:
+            response = await app.get("/building/1/local")
+        assert response.status_code == 200
+        assert response.json() == [
+            {"description": None, "id": 2, "number": 2},
+            {"description": None, "id": 1, "number": 17},
+        ]
+
+    async def test_update_not_existing_local(self, async_app):
+        payload = {
+            "number": "17",
+        }
+        async with async_app as app:
+            response = await app.patch("/local/1", json=payload)
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Local with id: 1 not found."}
+
+    @pytest.mark.usefixtures("locals_setup")
+    async def test_delete_local(self, async_app):
+        async with async_app as app:
+            response = await app.delete("/local/1")
+        assert response.status_code == 200
+        assert response.json() == {}
+        async with async_app as app:
+            response = await app.get("/building/1/local")
+        assert response.status_code == 200
+        assert response.json() == [{"description": None, "id": 2, "number": 2}]
+
+    async def test_delete_not_existing_local(self, async_app):
+        async with async_app as app:
+            response = await app.delete("/local/1")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Local with id: 1 not found."}
